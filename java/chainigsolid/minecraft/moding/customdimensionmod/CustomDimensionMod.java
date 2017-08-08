09b35a27b223604e4d401cc08e307184b3ea0417
chainingsolid <chainingsolid@users.noreply.github.com>
2017-08-08 18:13:43 -0400

    another test commit

---
+++ java/chainigsolid/minecraft/moding/customdimensionmod/CustomDimensionMod.java
@@ -0,0 +1,14 @@
+package chainigsolid.minecraft.moding.customdimensionmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = GlobalVariables.MOD_ID, version = GlobalVariables.MOD_VERSION)
+public class CustomDimensionMod {
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ java/chainigsolid/minecraft/moding/customdimensionmod/GlobalVariables.java
@@ -0,0 +1,10 @@
+package chainigsolid.minecraft.moding.customdimensionmod;
+
+public class GlobalVariables {
+	
+	public final static String MOD_ID = "chainingsolidcustomdimensionmod";
+	public final static String MOD_VERSION = "0.0.1";
+	
+	
+	
+}
